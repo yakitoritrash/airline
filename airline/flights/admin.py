e657f174db81a42e53a07fcbb9ab6cc6cd425c68
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-09-20 13:27:09 +0530

    almost done

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Flight, Airport, Passesnger
 # Register your models here.
+
+admin.site.register(Airport)
+
+admin.site.register(Flight) 
+ 
+admin.site.register(Passesnger)
