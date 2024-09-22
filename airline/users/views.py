75ba9ed6c0a199cb03521cb7f332dca2fa18a3a2
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-09-22 13:59:01 +0530

    done

--- airline/users/views.py
+++ airline/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -6,6 +7,7 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
     
     
     
@@ -13,7 +15,18 @@ def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("user_index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "Invalid credentials."
+            })
     return render(request, "users/login.html")
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged out."
+    })
